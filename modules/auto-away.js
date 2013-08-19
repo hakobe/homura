db772cf84b4fc312c8e3186bfa28cde621c69025
hakobe <hakobe@gmail.com>
2013-08-20 08:15:10 +0900

    auto away plugin

---
+++ modules/auto-away.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function AutoAway(options) {
+    this.message = options.message || 'Sorry, I am away from IRC.';
+}
+
+AutoAway.prototype.handleUserSession = function( userSession, bouncer ) {
+    userSession.on('attach', function() {
+        bouncer.ircClient.send( 'AWAY' ); // unaway
+    });
+    userSession.on('close', (function() {
+        if (!bouncer.isAttached()) {
+            bouncer.ircClient.send( 'AWAY', [this.message] ); // away
+        }
+    }).bind(this));
+};
+
+module.exports = AutoAway;
